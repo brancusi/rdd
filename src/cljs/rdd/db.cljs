a2b3c83820e73c651107d0bf188c2b1a8da4aa41
Aram Zadikian <az@whatisfood.com>
2021-02-28 16:24:33 -0800

    building costing

--- src/cljs/rdd/db.cljs
+++ src/cljs/rdd/db.cljs
@@ -1,49 +1,129 @@
 (ns rdd.db
-  (:require [re-frame.core :as rf]))
+  (:require [re-frame.core :as rf]
+            [taoensso.timbre :as timbre
+             :refer-macros [log info spy]]
+            [clojure.spec.alpha :as s]))
 
 (def default-db
   {:active-node "burrito"
-   :nodes
-   {"salt" {:name "Salt"
-            :id "salt"
-            :child-edges []}
 
-    "pepper" {:name "Pepper"
-              :id "pepper"
-              :child-edges []}
+   :nodes {"salt"         {:name "Salt"
+                           :id "salt"}
 
-    "paprika" {:name "Paprika"
-               :id "paprika"
-               :child-edges []}
+           "pepper"       {:name "Pepper"
+                           :id "pepper"}
 
-    "sauce-1" {:name "Sauce 1"
-               :id "sauce-1"
-               :child-edges ["sauce-1-salt" "sauce-1-pepper"]}
+           "paprika"      {:name "Paprika"
+                           :id "paprika"}
 
-    "sauce-2" {:name "Sauce 2"
-               :id "sauce-2"
-               :child-edges ["sauce-2-pepper" "sauce-2-paprika"]}
+           "sauce-1"      {:name "Sauce 1"
+                           :id "sauce-1"
+                           :child-edges ["sauce-1-salt" "sauce-1-pepper"]}
 
-    "sauce-blend" {:name "Sauce Blend"
-                   :id "sauce-blend"
-                   :child-edges ["sauce-blend-sauce-1" "sauce-blend-sauce-2"]}
+           "sauce-2"      {:name "Sauce 2"
+                           :id "sauce-2"
+                           :child-edges ["sauce-2-pepper" "sauce-2-paprika"]}
 
-    "burrito" {:name "Burrito"
-               :id "burrito"
-               :child-edges ["burrito-sauce-blend"]}}
+           "sauce-blend"  {:name "Sauce Blend"
+                           :id "sauce-blend"
+                           :child-edges ["sauce-blend-sauce-1" "sauce-blend-sauce-2"]}
 
-   :edges {"sauce-1-salt" {:child-node "salt" :qty 10 :order 1}
-           "sauce-1-pepper" {:child-node "pepper" :qty 10 :order 2}
+           "burrito"      {:name "Burrito"
+                           :id "burrito"
+                           :child-edges ["burrito-sauce-blend"]}}
 
-           "sauce-2-pepper" {:child-node "pepper" :qty 10 :order 1}
-           "sauce-2-paprika" {:child-node "paprika" :qty 10 :order 2}
+   :edges {"sauce-1-salt"         {:child-node "salt"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 1}
 
-           "sauce-blend-sauce-1" {:child-node "sauce-1" :qty 10 :order 1}
+           "sauce-1-pepper"       {:child-node "pepper"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 2}
 
-           "sauce-blend-sauce-2" {:child-node "sauce-2" :qty 10 :order 1}
+           "sauce-2-pepper"       {:child-node "pepper"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 1}
 
-           "burrito-sauce-blend" {:child-node "sauce-blend" :qty 10 :order 1}}})
+           "sauce-2-paprika"      {:child-node "paprika"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 2}
 
+           "sauce-blend-sauce-1"  {:child-node "sauce-1"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 1}
+
+           "sauce-blend-sauce-2"  {:child-node "sauce-2"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 1}
+
+           "burrito-sauce-blend"  {:child-node "sauce-blend"
+                                   :qty 10
+                                   :uom :gram
+                                   :order 1}}
+
+   :conversions {"salt" {:cup    {:gram 10}
+                         :case   {:pack 25}
+                         :pack   {:pound 1}}}
+
+   :costs {"salt"    [{:cost 10
+                       :qty 10
+                       :uom :pound
+                       :date 10000000000
+                       :additional-cost 10}]
+
+           "pepper"  [{:cost 30
+                       :qty 1
+                       :uom :case
+                       :date 10000000000
+                       :additional-cost 10}]
+
+           "paprika" [{:cost 40
+                       :qty 2
+                       :uom :case
+                       :date 10000000000
+                       :additional-cost 10}]}})
+
+(def hey {:cup    {:gram 10}
+          :case   {:pack 25}
+          :pack   {:pound 1}})
+
+
+(defn conversion-index
+  [conversion]
+  (reduce
+   (fn
+     [[acc] [from-uom val]]
+     (for [[to-uom qty] (seq val)]
+       (assoc-in acc [to-uom from-uom] (/ 1 qty))))
+   (list conversion)
+   (map (fn
+          [[key val]]
+          [key val]) conversion)))
+
+(comment
+  (conversion-index (get-in default-db [:conversions "salt"]))
+
+  ;; 
+  )
+
+
+(info "Acc" {:a 10})
+
+  ;; 
+
+
+(assoc-in {} [:salt :pack] 10)
+
+(seq {:a 10})
+
+(for [yo (keys {:a 10 :b 20})]
+  (info yo))
 
 (defn seed-db
   []
--- src/cljs/rdd/subs.cljs
+++ src/cljs/rdd/subs.cljs
@@ -2,6 +2,7 @@
   (:require
    [taoensso.timbre :as timbre
     :refer-macros [log info spy]]
+   [rdd.db :refer [default-db]]
    [re-frame.core :as rf :refer [subscribe reg-sub reg-sub-raw]]
    [reagent.ratom :as ra :refer [reaction]]))
 
@@ -17,6 +18,31 @@
    db))
 
 (reg-sub
+ :all-costs
+ (fn
+   [db _]
+   (:costs db)))
+
+(reg-sub
+ :uoms
+ (fn
+   [db _]
+   (:uoms db)))
+
+(reg-sub
+ :all-conversions
+ (fn
+   [db _]
+   (:conversions db)))
+
+(reg-sub
+ :conversion
+ :<- [:all-conversions]
+ (fn
+   [conversions [_ id]]
+   (get-in conversions [id])))
+
+(reg-sub
  :node
  (fn
    [db [_ id]]
@@ -27,6 +53,103 @@
  (fn
    [db [_ id]]
    (get-in db [:edges id])))
+
+(reg-sub
+ :cost
+ (fn
+   [db [_ id]]
+   (get-in db [:costs id])))
+
+(reg-sub
+ :cost
+ (fn
+   [db [_ id]]
+   (get-in db [:costs id])))
+
+(reg-sub
+ :cost-for-uom
+ (fn [[_ node-id _]]
+   [(subscribe [:cost node-id])
+    (subscribe [:uoms])
+    (subscribe [:conversion])])
+ (fn
+   [[cost uoms conversion] [_ _ to-uom]]))
+
+
+(defn convert-uom->uom
+  "Returns the conversion ratio from one UOM another"
+  [from to]
+  20)
+
+(def standard-uoms
+  {:default-weight-type :gram
+   :default-volume-type :cup
+   :default-unit-type :each
+
+   :by-category {:weight [:gram :kilogram :pound]
+                 :volume [:cup]
+                 :count [:each]}
+
+   :by-type {:gram      {:type :weight
+                         :factor 1.0}
+
+             :kilogram  {:type :weight
+                         :factor 1000.0}
+
+             :pound     {:type :weight
+                         :factor 453.5}
+
+             :cup       {:type :volume
+                         :factor 1.0}
+
+             :each      {:type :count}}
+   :all-types [:gram :kilogram :pound :cup]})
+
+(defn build-conversion-path
+  [node-id from-uom to-uom]
+  [])
+
+(defn cost-for-uom
+  [node-id cost conversion to-uom]
+
+  ;; Normalize against base UOM
+  (let [sorted-cost (sort-by :date cost)
+        recent-cost (first sorted-cost)
+        {:keys [cost uom qty]} recent-cost
+        standard-uom (get-in standard-uoms [:by-type uom])]
+
+    ;; Convert to target UOM
+    ;; Try to find in standard uoms  
+    (if (some? standard-uom)
+
+      ;; This is a standard UOM can normalize
+      (let [factor (:factor standard-uom)
+            normalized-cost-per-uom (/ cost qty factor)]
+
+        normalized-cost-per-uom)
+
+      ;; Not a standard UOM, needs conversion
+      20
+
+      ;; 
+      )
+
+    ;; 
+    ))
+
+
+(def cost (get-in default-db [:costs "salt"]))
+(def conversion (get-in default-db [:conversions "salt"]))
+(def to-uom :gram)
+
+(comment
+  (/ 10 10 453)
+
+  (cost-for-uom node-id cost conversion to-uom)
+
+  ;; 
+  )
+
 
 (reg-sub
  :child-node
@@ -41,7 +164,7 @@
    (reaction
     (let [node @(subscribe [:node node-id])
           children (mapv (fn [edge-id] @(subscribe [:edge->child edge-id])) (:child-edges node))]
-      (if (not (empty? children))
+      (if (not-empty children)
 
         ;; Has children
         (-> node
@@ -49,7 +172,11 @@
             (merge {:children children}))
 
         ;; Terminate hit bottom
-        node)))))
+        (let [cost @(subscribe [:cost node-id])
+              yo @(subscribe [:cost-for-uom node-id :gram])]
+          (info "Yoson ")
+          (-> node
+              (merge {:cost yo}))))))))
 
 ;; Given an edge, we materialize a child node
 (reg-sub-raw
@@ -57,34 +184,11 @@
  (fn [_ [_ edge-id]]
    (reaction
     (let [edge @(subscribe [:edge edge-id])
-          node @(subscribe [:node->tree (:child-node edge)])
-          qty (:qty edge)
-          order (:order edge)]
+          node @(subscribe [:node->tree (:child-node edge)])]
 
       (-> node
           (dissoc :child-edges)
-          (merge {:qty qty :order order}))))))
-
-(comment
-  (dissoc {:a 10 :b 20} :a)
-  (->> 10
-       (- 20))
-
-  (-> 10
-      (- 20))
-
-  (->> {:a 10}
-       (merge {:a 20}))
-
-  (merge {:a 10} {:a 20})
-
-  (-> {:a 10}
-      (merge {:a 20}))
-
-  (- 20 10)
-  (- 10 20))
-
-
+          (merge edge))))))
 
 (reg-sub
  :active-node-id
